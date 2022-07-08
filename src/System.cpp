f3d19ccfad3c19996615898229ed4d54cd594114
Romain TISSERAND <romain.tisserand@gmail.com>
2022-07-08 12:09:00 +0200

    Fix system.cpp

--- src/System.cpp
+++ src/System.cpp
@@ -9,10 +9,10 @@ System::System(QObject *parent)
 
 void System::reboot() const
 {
-    QProcess::startDetached(QLatin1String("reboot"));
+    QProcess::startDetached(QString("reboot"));
 }
 
 void System::shutdown() const
 {
-    QProcess::startDetached(QLatin1String("shutdown -h now"));
+    QProcess::startDetached(QString("shutdown"), {"-h", "now"});
 }
